5764c1fd2ca063c1d4b18202337c1fa72d8c19d6
FlorianFlorianFlorianFlorian <florian.kugler@stud.fh-campuswien.ac.at>
2023-10-20 20:25:45 +0200

    add basic Menu

--- src/main/java/mill/MCP.java
+++ src/main/java/mill/MCP.java
@@ -1,4 +1,9 @@
 package mill;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MCP {
     static void printBoard(){
         System.out.println("X----------X----------X");
@@ -17,11 +22,28 @@ public class MCP {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String firstinput;
+
         System.out.println("Welcome to the Mill Game");
         System.out.println("Start the Tutorial --> press t");
         System.out.println("Start a new Game   --> press g");
 
-        //printBoard();
+        firstinput = br.readLine();
+
+        if (firstinput.equals("g")) {
+            System.out.println("Wir beginnen das Spiel!");
+
+        }
+        else if (firstinput.equals("t")){
+            System.out.println("Hier das Tutorial:");
+            printBoard();
+        }
+        else {
+            System.out.println("Eingabe ungültig. Auf Wiedersehen");
+        }
+
+
     }
 }
