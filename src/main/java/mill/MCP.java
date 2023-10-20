0b24f40f24f3c6e84293cc65f9ea1640f0c34877
FlorianFlorianFlorianFlorian <florian.kugler@stud.fh-campuswien.ac.at>
2023-10-20 19:53:37 +0200

    add useless print

--- src/main/java/mill/MCP.java
+++ src/main/java/mill/MCP.java
@@ -4,5 +4,6 @@ public class MCP {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("and here we go");
+        System.out.println("das ist noch ein Test");
     }
 }
