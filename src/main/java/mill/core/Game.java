e8d0c1520b6eb8e8880dfb6d26caa5f825e37996
FlorianFlorianFlorianFlorian <florian.kugler@stud.fh-campuswien.ac.at>
2023-11-24 17:12:45 +0100

    implement checking for Mill. at the time everything is hardcoded! PLEASE REWORK AND TEST.

--- src/main/java/mill/core/Game.java
+++ src/main/java/mill/core/Game.java
@@ -21,10 +21,10 @@ public class Game {
 
     public String getPlayer(){
         if(turn == Player.OOO){
-            return "weiß O";
+            return "weiß ('O')";
         }
         else{
-            return "schwarz X";
+            return "schwarz ('X')";
         }
     }
 
@@ -69,6 +69,12 @@ public class Game {
             return false;
         }
 
+        /*
+        Bug Report:
+        when wanting to place a token at a6 it is placed to a5 put it is shown at a6 and m6.
+        check out why!
+         */
+
         chooseField[index] = token;
         PlaceCounter++;
         switchPlayer();
@@ -100,12 +106,129 @@ public class Game {
             BLACK -> 'X';
         */
 
-        // work in progress
+        // STAND AKUTELL: stupideste Logik ever
+        // NEEDS SINCERE REFACTORING
 
-        // outer array
-        if(outer[6].equals(Field.BLACK) && outer[7].equals(Field.BLACK)){
+        /*
+        0   1   2               1   2   3
+        3       4               4       5
+        5   6   7               6   7   8
+         */
+
+        // WHITE
+        //          OUTER
+        if(outer[0].equals(Field.WHITE) && outer[1].equals(Field.WHITE) && outer[2].equals(Field.WHITE)){
             return true;
         }
+        if(outer[0].equals(Field.WHITE) && outer[3].equals(Field.WHITE) && outer[5].equals(Field.WHITE)){
+            return true;
+        }
+        if(outer[2].equals(Field.WHITE) && outer[4].equals(Field.WHITE) && outer[8].equals(Field.WHITE)){
+            return true;
+        }
+        if(outer[5].equals(Field.WHITE) && outer[6].equals(Field.WHITE) && outer[7].equals(Field.WHITE)){
+            return true;
+        }
+
+        //          MIDDLE
+        if(middle[0].equals(Field.WHITE) && middle[1].equals(Field.WHITE) && middle[2].equals(Field.WHITE)){
+            return true;
+        }
+        if(middle[0].equals(Field.WHITE) && middle[3].equals(Field.WHITE) && middle[5].equals(Field.WHITE)){
+            return true;
+        }
+        if(middle[2].equals(Field.WHITE) && middle[4].equals(Field.WHITE) && middle[8].equals(Field.WHITE)){
+            return true;
+        }
+        if(middle[5].equals(Field.WHITE) && middle[6].equals(Field.WHITE) && middle[7].equals(Field.WHITE)){
+            return true;
+        }
+
+        //          INNER
+        if(inner[0].equals(Field.WHITE) && inner[1].equals(Field.WHITE) && inner[2].equals(Field.WHITE)){
+            return true;
+        }
+        if(inner[0].equals(Field.WHITE) && inner[3].equals(Field.WHITE) && inner[5].equals(Field.WHITE)){
+            return true;
+        }
+        if(inner[2].equals(Field.WHITE) && inner[4].equals(Field.WHITE) && inner[8].equals(Field.WHITE)){
+            return true;
+        }
+        if(inner[5].equals(Field.WHITE) && inner[6].equals(Field.WHITE) && inner[7].equals(Field.WHITE)){
+            return true;
+        }
+        // DIAGONALEN
+        if(outer[1].equals(Field.WHITE) && middle[1].equals(Field.WHITE) && inner[1].equals(Field.WHITE)){
+            return true;
+        }
+        if(outer[3].equals(Field.WHITE) && middle[3].equals(Field.WHITE) && inner[3].equals(Field.WHITE)){
+            return true;
+        }
+        if(outer[4].equals(Field.WHITE) && middle[4].equals(Field.WHITE) && inner[4].equals(Field.WHITE)){
+            return true;
+        }
+        if(outer[6].equals(Field.WHITE) && middle[6].equals(Field.WHITE) && inner[6].equals(Field.WHITE)){
+            return true;
+        }
+
+        // BLACK
+        //          OUTER
+        if(outer[0].equals(Field.BLACK) && outer[1].equals(Field.BLACK) && outer[2].equals(Field.BLACK)){
+            return true;
+        }
+        if(outer[0].equals(Field.BLACK) && outer[3].equals(Field.BLACK) && outer[5].equals(Field.BLACK)){
+            return true;
+        }
+        if(outer[2].equals(Field.BLACK) && outer[4].equals(Field.BLACK) && outer[8].equals(Field.BLACK)){
+            return true;
+        }
+        if(outer[5].equals(Field.BLACK) && outer[6].equals(Field.BLACK) && outer[7].equals(Field.BLACK)){
+            return true;
+        }
+
+        //          MIDDLE
+        if(middle[0].equals(Field.BLACK) && middle[1].equals(Field.BLACK) && middle[2].equals(Field.BLACK)){
+            return true;
+        }
+        if(middle[0].equals(Field.BLACK) && middle[3].equals(Field.BLACK) && middle[5].equals(Field.BLACK)){
+            return true;
+        }
+        if(middle[2].equals(Field.BLACK) && middle[4].equals(Field.BLACK) && middle[8].equals(Field.BLACK)){
+            return true;
+        }
+        if(middle[5].equals(Field.BLACK) && middle[6].equals(Field.BLACK) && middle[7].equals(Field.BLACK)){
+            return true;
+        }
+
+        //          INNER
+        if(inner[0].equals(Field.BLACK) && inner[1].equals(Field.BLACK) && inner[2].equals(Field.BLACK)){
+            return true;
+        }
+        if(inner[0].equals(Field.BLACK) && inner[3].equals(Field.BLACK) && inner[5].equals(Field.BLACK)){
+            return true;
+        }
+        if(inner[2].equals(Field.BLACK) && inner[4].equals(Field.BLACK) && inner[8].equals(Field.BLACK)){
+            return true;
+        }
+        if(inner[5].equals(Field.BLACK) && inner[6].equals(Field.BLACK) && inner[7].equals(Field.BLACK)){
+            return true;
+        }
+
+        // DIAGONAEL
+        if(outer[1].equals(Field.BLACK) && middle[1].equals(Field.BLACK) && inner[1].equals(Field.BLACK)){
+            return true;
+        }
+        if(outer[3].equals(Field.BLACK) && middle[3].equals(Field.BLACK) && inner[3].equals(Field.BLACK)){
+            return true;
+        }
+        if(outer[4].equals(Field.BLACK) && middle[4].equals(Field.BLACK) && inner[4].equals(Field.BLACK)){
+            return true;
+        }
+        if(outer[6].equals(Field.BLACK) && middle[6].equals(Field.BLACK) && inner[6].equals(Field.BLACK)){
+            return true;
+        }
+
+
 
 
         return false;
